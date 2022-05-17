27c907b7e3458842fe854c36c3b4eba8c918a2c4
Antoine Puech <antoine.puech@edu.devinci.fr>
2022-05-17 10:44:04 +0200

    chore(jest.config.js turbo.json): refactor jest config and add test command to turbo.json

--- apps/api/jest.config.js
+++ apps/api/jest.config.js
@@ -1,14 +1,5 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 export default {
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
-  extensionsToTreatAsEsm: ['.ts'],
-  globals: {
-    'ts-jest': {
-      useESM: true,
-    },
-  },
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
-  },
 };
