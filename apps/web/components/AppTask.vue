b2a22a9d9db819ee0c6758c3ff67f4f151378e30
Antoine Puech <antoine.puech@edu.devinci.fr>
2022-05-18 11:06:56 +0200

    feat(web): create column and task components, installed vue draggable

---
+++ apps/web/components/AppTask.vue
@@ -0,0 +1,26 @@
+<script setup>
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="card w-full h-48 bg-secondary-content shadow-xl">
+    <div class="card-body">
+      <h2 class="card-title text-sm">
+        {{ props.title }}
+      </h2>
+      <p class="text-xs">
+        If a dog chews shoes whose shoes does he choose?
+      </p>
+      <div class="card-actions justify-end">
+        <button class="btn btn-primary">
+          Done
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
--- apps/web/pages/index.vue
+++ apps/web/pages/index.vue
@@ -1,5 +1,17 @@
+<script setup>
+
+const columns = ref([
+  { title: 'First title' },
+  { title: 'Second title' },
+  { title: 'Third title' },
+]);
+
+</script>
+
 <template>
-  <AppCard />
+  <div class="flex">
+    <AppColumn v-for="(column, key) in columns" :key="key" :title="column.title" />
+  </div>
 </template>
 <script setup>
 </script>
